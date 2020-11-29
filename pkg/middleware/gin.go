a0392d3be167413eecd8406f1817f18804a26a04
andefined <dimitris.papaevagelou@andefined.com>
2020-11-29 18:48:59 +0200

    added new graph

--- pkg/middleware/gin.go
+++ pkg/middleware/gin.go
@@ -15,7 +15,7 @@ func EnableCORS(cors string) gin.HandlerFunc {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", cors)
 		c.Writer.Header().Set("Access-Control-Max-Age", "86400")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", " X-Requested-With, Content-Type, Origin, Accept, Client-Security-Token, Accept-Encoding, Authorization")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Origin, Accept, Client-Security-Token, Accept-Encoding, Authorization")
 		c.Writer.Header().Set("Access-Control-Expose-Headers", "Content-Length, Authorization")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 
